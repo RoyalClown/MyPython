93317f633ebbe05772821f34ea78c652f61f900e
RoyalClown <421581115@qq.com>
2017-02-24 11:25:33 +0800

    Personal Commit

---
+++ DBCurrency/Template/fileSystem.py
@@ -0,0 +1,12 @@
+"""
+    @description:   
+    @author:        RoyalClown
+    @date:          2017/2/24
+"""
+
+class FileSystem:
+    def __init__(self):
+        self.post_url = "http://10.10.100.200:9999/file/upload"
+
+
+
