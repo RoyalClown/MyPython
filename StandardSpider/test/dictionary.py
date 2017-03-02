a47b7d658007b9abebfdf52bb2a60e969284e591
RoyalClown <421581115@qq.com>
2017-03-02 16:25:54 +0800

    Personal Commit

--- StandardSpider/test/dictionary.py
+++
@@ -1,9 +0,0 @@
-"""
-    @description:   
-    @author:        RoyalClown
-    @date:          2016/11/17
-"""
-a = {"a": 13}
-print(isinstance(a, dict))
-b = a.items()
-print()
--- StandardSpider/test/for_else.py
+++
@@ -1,5 +0,0 @@
-import json
-
-string = "a:21:3d"
-a = string.split(":", 1)
-print(a)
